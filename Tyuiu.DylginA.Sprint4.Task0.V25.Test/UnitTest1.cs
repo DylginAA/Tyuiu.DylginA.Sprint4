ebc3b8a3f62e582f507c69fc7f6e801b23c1bcee
Admin <Admin@HOME-PC>
2024-11-10 14:57:37 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.DylginA.Sprint4.Task0.V25.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.DylginA.Sprint4.Task0.V25.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.DylginA.Sprint4.Task0.V25/Program.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.DylginA.Sprint4.Task0.V25.Lib;
+namespace Tyuiu.DylginA.Sprint4.Task0.V25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил: Дылгин А. | ИБКСБ-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4");
+            Console.WriteLine("* Тема: Одномернные массивы");
+            Console.WriteLine("* Задание #0");
+            Console.WriteLine("* Вариант #25");
+            Console.WriteLine("* Выполнил: Дылгин А. | ИБКСБ-24-1");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:Дан одномерный целочисленный массив на 10 элементов заполненный статическими значениями в диапазоне от 0 до 9 подсчитать сумму четных элементов массива.  {1, 4, 2, 6, 4, 8, 6, 7, 8, 4}");
+            Console.WriteLine("");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:");
+            Console.WriteLine("***************************************************************************");
+            int[] array = new int[] { 1, 4, 2, 6, 4, 8, 6, 7, 8, 4 };
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.GetSumEvenArrEl(array));
+        }
+    }
+}
