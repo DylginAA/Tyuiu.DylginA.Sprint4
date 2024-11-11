b99f55b732a2f76d052a9ee87db270e17dd25de1
Admin <Admin@HOME-PC>
2024-11-11 18:50:25 +0500

    Task5

---
+++ Tyuiu.DylginA.Sprint4.Task5.V14.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.DylginA.Sprint4.Task5.V14.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.DylginA.Sprint4.Task5.V14/Program.cs
@@ -0,0 +1,49 @@
+﻿using Tyuiu.DylginA.Sprint4.Task5.V14.Lib;
+namespace Tyuiu.DylginA.Sprint4.Task5.V14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил: Дылгин А. | ИБКСБ-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4");
+            Console.WriteLine("* Тема: Одномернные массивы");
+            Console.WriteLine("* Задание #5");
+            Console.WriteLine("* Вариант #14");
+            Console.WriteLine("* Выполнил: Дылгин А. | ИБКСБ-24-1");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:Дан двумерный целочисленный массив 5 на 5 элементов, заполненный случайными значениями в диапазоне от -5 до 7. " +
+                "Найти количество положительных элементов.");
+            Console.WriteLine("");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:");
+            Console.WriteLine("***************************************************************************");
+            int[,] array = new int[5, 5];
+            Random random = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    array[i, j] = random.Next(-5, 7);
+                }
+            }
+            Console.WriteLine("\nМассив:");
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write($"{array[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ");
+            Console.WriteLine("***************************************************************************");
+            int res = ds.Calculate(array);
+            Console.WriteLine(res);
+        }
+    }
+}
