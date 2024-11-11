e8bd038604dbe4bb2748441d097f68c03cb323d7
Admin <Admin@HOME-PC>
2024-11-11 18:28:09 +0500

    Task4

---
+++ Tyuiu.DylginA.Sprint4.Task4.V23/Program.cs
@@ -0,0 +1,48 @@
+﻿using Tyuiu.DylginA.Sprint4.Task4.V23.Lib;
+namespace Tyuiu.DylginA.Sprint4.Task4.V23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил: Дылгин А. | ИБКСБ-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4");
+            Console.WriteLine("* Тема: Одномернные массивы");
+            Console.WriteLine("* Задание #4");
+            Console.WriteLine("* Вариант #23");
+            Console.WriteLine("* Выполнил: Дылгин А. | ИБКСБ-24-1");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:Дан двумерный целочисленный массив 5 на 5 элементов, заполненный значениями с клавиатуры в диапазоне от 3 до 8. " +
+                "Найдите сумму нечетных элементов массива.");
+            Console.WriteLine("");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:");
+            Console.WriteLine("***************************************************************************");
+            int[,] array = new int[5,5];
+            for (int i = 0; i < 5; i++)
+            {
+                for(int j=0; j < 5; j++)
+                {
+                    Console.WriteLine($"Введите значение {i},{j} элемента массива");
+                    array[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("Массив:");
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.WriteLine(array[i, j] + "\t");
+                }
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ");
+            Console.WriteLine("***************************************************************************");
+            int res = ds.Calculate(array);
+            Console.WriteLine(res);
+        }
+    }
+}
